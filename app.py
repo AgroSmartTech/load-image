61b2da71e672a3bd0d1a2ecf7d07b4da20c2d67f
Oleh Plotnikov <oleh.plotnikov90@gmail.com>
2024-05-10 15:24:17 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# from flask import Flask
+from PIL import Image
+from PIL import ImageShow
+
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def hello_world():
+    # return 'Hello World!'
+
+image_path = "/usr/bin/image.png"
+
+image = Image.open(image_path)
+
+# Display the PNG image
+ImageShow.show(image)
+
+# Keep the script running until the user closes the image
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    pass
